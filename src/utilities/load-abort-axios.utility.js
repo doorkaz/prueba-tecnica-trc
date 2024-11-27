f46cdaba6a075c4ae76ac6b4a3aeef52e1219991
doorkaz <retamalespj@gmail.com>
2024-11-27 14:59:08 -0300

    Comentario explicativo en load-abort-axios

--- src/utilities/load-abort-axios.utility.js
+++ src/utilities/load-abort-axios.utility.js
@@ -1,3 +1,5 @@
+// AbortController permite abortar una o más solicitudes cuando sea necesario
+// Se usa para cancelar requests del fetch al servicio que no van a ser usadas
 export const loadAbort = () => {
   const controller = new AbortController()
   return controller
